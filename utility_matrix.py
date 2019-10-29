73947074b7acf27a056cc89180b3d26baeb0c2ae
iamkroot <kroot.patel@gmail.com>
2019-10-29 15:30:41 +0530

    Update UtilityMatrix generation logic

--- utility_matrix.py
+++ utility_matrix.py
@@ -1,57 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 28 20:28:49 2019
-
-@author: Sheth_Smit
-"""
-
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+from pathlib import Path
+from dataset_handler import DatasetHandler
+
 
 class UtilityMatrix:
-    
+    """Represents the utility matrix of Users x Movies"""
+
+    UTILITY_MAT_PATH = Path("data", "utility_matrix.csv")
+
     def __init__(self):
-        
-        ratings_file = open('./ml-1m/ratings.dat', 'r')
-        users_file = open('./ml-1m/users.dat', 'r')
-        movies_file = open('./ml-1m/movies.dat', 'r')
+        self.dh = DatasetHandler()
+        self._utility_mat = None
 
-        self.ratings = list(ratings_file)
-        self.users = list(users_file)
-        self.movies = list(movies_file)
-        
-        self.ratings_train, self.ratings_test = train_test_split(self.ratings, test_size = 0.2, random_state=9)
-        
-        ratings_file.close()
-        users_file.close()
-        movies_file.close()
-    
-    def generate_matrix(self, ratings_model):
-        mat = [[0 for j in range(3952)] for i in range(6041)]
-        
-        cnt = 0
-        for movie in self.movies:
-            movie_name = movie.split("::")[1]
-            mat[0][cnt] = movie_name
-            cnt = cnt+1
-            
-        for line in ratings_model:
-            row = line.split("::")
-            mat[int(row[0])][int(row[1])-1] = int(row[2])
+    @property
+    def utility_mat(self) -> pd.DataFrame:
+        if self._utility_mat is None:
+            if self.UTILITY_MAT_PATH.exists():
+                self._utility_mat = pd.read_csv(self.UTILITY_MAT_PATH)
+            else:
+                self._utility_mat = self.generate_matrix()
+        return self._utility_mat
 
-        return mat
-    
-    def generate_csv(self):
-        
-        mat_train = self.generate_matrix(self.ratings_train)
-        pd.DataFrame(mat_train).to_csv("train_utility_matrix.csv")
-        print("Created train csv")
-        
-        mat_test = self.generate_matrix(self.ratings_test)
-        pd.DataFrame(mat_test).to_csv("test_utility_matrix.csv")
-        print("Created test csv")
-        
-um = UtilityMatrix()
-um.generate_csv()
-    
+    def generate_matrix(self) -> pd.DataFrame:
+        print("Generating utility matrix")
+        self._utility_mat = self.dh.train_ratings.pivot(
+            index="user_id", columns="movie_id", values="rating"
+        ).reindex(columns=sorted(self.dh.full_ratings.movie_id.unique()))
+        self._utility_mat.to_csv(self.UTILITY_MAT_PATH, index=False)
+        return self._utility_mat
+
+
+if __name__ == "__main__":
+    um = UtilityMatrix()
+    print(um.utility_mat)
