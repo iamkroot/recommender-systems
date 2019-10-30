3f6222e15de9ec8258fef87684758a39e6cc57a4
iamkroot <kroot.patel@gmail.com>
2019-10-30 20:12:38 +0530

    collab_filter: Format files using black

--- collab_filter.py
+++ collab_filter.py
@@ -3,50 +3,52 @@ import pandas as pd
 import math
 from numpy.linalg import norm
 
-class Collab_Filter:
 
-    def __init__(self,path):
-        self.path=path
-        self.umDf = pd.read_csv(path,low_memory=False)
+class Collab_Filter:
+    def __init__(self, path):
+        self.path = path
+        self.umDf = pd.read_csv(path, low_memory=False)
         self.user_mean = [0 for j in range(6041)]
         self.um = self.umDf.values
         self.movies = list(self.umDf.columns)
         # print(len(self.movies))
-        for i in range(1,6041):
-            m=0
+        for i in range(1, 6041):
+            m = 0
             cnt = 0
-            for j in range(1,len(self.movies)) :
-                if not math.isnan(self.um[i][j]) :
-                    m=m+self.um[i][j]
-                    cnt+=1
-            self.user_mean[i]=m/cnt
-            for j in range(1,len(self.movies)):
+            for j in range(1, len(self.movies)):
                 if not math.isnan(self.um[i][j]):
-                    self.um[i][j] = self.um[i][j]-self.user_mean[i]
+                    m = m + self.um[i][j]
+                    cnt += 1
+            self.user_mean[i] = m / cnt
+            for j in range(1, len(self.movies)):
+                if not math.isnan(self.um[i][j]):
+                    self.um[i][j] = self.um[i][j] - self.user_mean[i]
                 else:
                     self.um[i][j] = 0
         # print(self.um[1][:],self.user_mean[1])
         self.um = np.array(self.um)
 
-    def item_sim(self,i,j):
-        return np.dot(self.um[1:,i],self.um[1:,j])/(norm(self.um[1:,i])*norm(self.um[1:,j]))
+    def item_sim(self, i, j):
+        return np.dot(self.um[1:, i], self.um[1:, j]) / (
+            norm(self.um[1:, i]) * norm(self.um[1:, j])
+        )
 
-    def top_sim_items(self,u,i):
+    def top_sim_items(self, u, i):
         ti = []
-        for j in range(1,len(self.movies)):
-            if j!=i and self.um[u][j]!=0 :
-                ti.append((self.item_sim(i,j),j))
+        for j in range(1, len(self.movies)):
+            if j != i and self.um[u][j] != 0:
+                ti.append((self.item_sim(i, j), j))
         ti.sort(reverse=True)
         return ti[:15]
 
-    def predict_rating(self,u,m):
+    def predict_rating(self, u, m):
         m = self.movies.index(str(m))
-        ti = self.top_sim_items(u,m)
-        num,den = 0,0
+        ti = self.top_sim_items(u, m)
+        num, den = 0, 0
         for x in ti:
-            num+=x[0]*self.um[u][x[1]]
-            den+=x[0]
-        r = num/den
+            num += x[0] * self.um[u][x[1]]
+            den += x[0]
+        r = num / den
         r = r + self.user_mean[u]
         return r
 
@@ -55,5 +57,5 @@ if __name__ == "__main__":
     cf = Collab_Filter("./data/utility_matrix.csv")
     # for x in cf.top_sim_items(1,1):
     #     print(x,cf.movies[x[1]])
-    print("Rating: ",cf.predict_rating(970,2194))
+    print("Rating: ", cf.predict_rating(970, 2194))
     # a = cf.self.
--- collab_filter_baseline.py
+++ collab_filter_baseline.py
@@ -3,11 +3,11 @@ import pandas as pd
 import math
 from numpy.linalg import norm
 
-class CF_Baseline:
 
-    def __init__(self,path):
-        self.path=path
-        self.umDf = pd.read_csv(path,low_memory=False)
+class CF_Baseline:
+    def __init__(self, path):
+        self.path = path
+        self.umDf = pd.read_csv(path, low_memory=False)
         self.user_mean = [0 for j in range(6041)]
         self.um = self.umDf.values
         self.movies = list(self.umDf.columns)
@@ -15,61 +15,64 @@ class CF_Baseline:
         self.movie_mean = [0 for j in range(len(self.movies))]
         self.global_mean = 0
         self.global_count = 0
-        for i in range(1,6041):
-            m=0
+        for i in range(1, 6041):
+            m = 0
             cnt = 0
-            for j in range(1,len(self.movies)) :
-                if not math.isnan(self.um[i][j]) :
-                    m=m+self.um[i][j]
-                    cnt+=1
-            self.user_mean[i]=m/cnt
-            self.global_mean+=m
-            self.global_count+=cnt
-            for j in range(1,len(self.movies)):
+            for j in range(1, len(self.movies)):
                 if not math.isnan(self.um[i][j]):
-                    self.um[i][j] = self.um[i][j]#-self.user_mean[i]
+                    m = m + self.um[i][j]
+                    cnt += 1
+            self.user_mean[i] = m / cnt
+            self.global_mean += m
+            self.global_count += cnt
+            for j in range(1, len(self.movies)):
+                if not math.isnan(self.um[i][j]):
+                    self.um[i][j] = self.um[i][j]  # -self.user_mean[i]
                 else:
                     self.um[i][j] = 0
-        self.global_mean = self.global_mean/self.global_count
-        for j in range(1,len(self.movies)):
-            m=0
-            cnt=0
-            for i in range(1,6041):
+        self.global_mean = self.global_mean / self.global_count
+        for j in range(1, len(self.movies)):
+            m = 0
+            cnt = 0
+            for i in range(1, 6041):
                 if self.um[i][j] != 0:
-                    m=m+self.um[i][j]
-                    cnt=cnt+1
+                    m = m + self.um[i][j]
+                    cnt = cnt + 1
             if cnt != 0:
-                self.movie_mean[j] = m/cnt
+                self.movie_mean[j] = m / cnt
         # print(self.um[1][:],self.user_mean[1])
         self.um = np.array(self.um)
 
-    def item_sim(self,i,j):
-        return np.dot(self.um[1:,i],self.um[1:,j])/(norm(self.um[1:,i])*norm(self.um[1:,j]))
+    def item_sim(self, i, j):
+        return np.dot(self.um[1:, i], self.um[1:, j]) / (
+            norm(self.um[1:, i]) * norm(self.um[1:, j])
+        )
 
-    def top_sim_items(self,u,i):
+    def top_sim_items(self, u, i):
         ti = []
-        for j in range(1,len(self.movies)):
-            if j!=i and self.um[u][j]!=0 :
-                ti.append((self.item_sim(i,j),j))
+        for j in range(1, len(self.movies)):
+            if j != i and self.um[u][j] != 0:
+                ti.append((self.item_sim(i, j), j))
         ti.sort(reverse=True)
         return ti[:15]
 
-    def baseline(self,u,m):
+    def baseline(self, u, m):
         return self.user_mean[u] + self.movie_mean[m] - self.global_mean
 
-    def predict_rating(self,u,m):
+    def predict_rating(self, u, m):
         m = self.movies.index(str(m))
-        ti = self.top_sim_items(u,m)
-        num,den = 0,0
+        ti = self.top_sim_items(u, m)
+        num, den = 0, 0
         for x in ti:
-            num+=x[0]*(self.um[u][x[1]] - self.baseline(u,x[1]))
-            den+=x[0]
-        r = num/den
-        r = r + self.baseline(u,m)
+            num += x[0] * (self.um[u][x[1]] - self.baseline(u, x[1]))
+            den += x[0]
+        r = num / den
+        r = r + self.baseline(u, m)
         return r
+
 
 if __name__ == "__main__":
     cfb = CF_Baseline("./data/utility_matrix.csv")
     # for x in cf.top_sim_items(1,1):
     #     print(x,cf.movies[x[1]])
-    print("Rating: ",cfb.predict_rating(3589,1562))
+    print("Rating: ", cfb.predict_rating(3589, 1562))
